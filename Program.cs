76cd1c2aa03144b351c7a7b6e7c900bf6a8330e8
Anders Lundholm Fors <mr.fors@gmail.com>
2023-03-24 12:47:20 +0100

    ersatte enbokstavsvariabler med mer beskrivande namn

--- Program.cs
+++ Program.cs
@@ -76,10 +76,10 @@
                     else if(argument.Length == 1) // FIXME: System.NullReferenceException när inge lista laddats
                     {
                         Console.WriteLine("Write word in Swedish: ");
-                        string s = Console.ReadLine();
+                        string swedish = Console.ReadLine();
                         Console.Write("Write word in English: ");
-                        string e = Console.ReadLine();
-                        dictionary.Add(new SweEngGloss(s, e));
+                        string english = Console.ReadLine();
+                        dictionary.Add(new SweEngGloss(swedish, english));
                     }
 
                     // FIXME: Meddela om och varför inget lades till vid t ex endast 1 argument
@@ -100,14 +100,14 @@
                     else if (argument.Length == 1)
                     {
                         Console.WriteLine("Write word in Swedish: ");
-                        string s = Console.ReadLine();
+                        string swedish = Console.ReadLine();
                         Console.Write("Write word in English: ");
-                        string e = Console.ReadLine();
+                        string english = Console.ReadLine();
                         int index = -1;
                         for (int i = 0; i < dictionary.Count; i++)
                         {
                             SweEngGloss gloss = dictionary[i];
-                            if (gloss.word_swe == s && gloss.word_eng == e)
+                            if (gloss.word_swe == swedish && gloss.word_eng == english)
                                 index = i;
                         }
                         dictionary.RemoveAt(index);
@@ -129,12 +129,12 @@
                     else if (argument.Length == 1)
                     {
                         Console.WriteLine("Write word to be translated: ");
-                        string s = Console.ReadLine();
+                        string swedish = Console.ReadLine();
                         foreach (SweEngGloss gloss in dictionary)
                         {
-                            if (gloss.word_swe == s)
+                            if (gloss.word_swe == swedish)
                                 Console.WriteLine($"English for {gloss.word_swe} is {gloss.word_eng}");
-                            if (gloss.word_eng == s)
+                            if (gloss.word_eng == swedish)
                                 Console.WriteLine($"Swedish for {gloss.word_eng} is {gloss.word_swe}");
                         }
                     }
